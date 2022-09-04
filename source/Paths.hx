6a14e75ccbae54a9ec2c244bac3eac7f3cfda29f
kamodrip <100513117+Akhia11@users.noreply.github.com>
2022-09-04 16:25:47 +0300

    Skill issue lmaoaoaao

--- source/Paths.hx
+++ source/Paths.hx
@@ -221,8 +221,10 @@ class Paths
 	inline static public function voices(song:String, suffix:String = ''):Any
 	{
 		var songKey:String = '${formatToSongPath(song)}/Voices'+suffix;		
-		if (PlayState.god)			songKey = '${formatToSongPath(song)}/Voices-god'+suffix;
-		if (PlayState.trueDiff)			songKey = '${formatToSongPath(song)}/Voices-true'+suffix;
+		if (PlayState.god)			
+                songKey = '${formatToSongPath(song)}/Voices-god'+suffix;
+		if (PlayState.trueDiff)			
+                songKey = '${formatToSongPath(song)}/Voices-true'+suffix;
 		return voices;
 	}
 
