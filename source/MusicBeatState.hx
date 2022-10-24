cc1411fa6475f10d7d359ebf788ad7acd212ac12
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-10-24 11:11:00 +0800

    Update MusicBeatState.hx

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -13,6 +13,12 @@ import flixel.util.FlxColor;
 import flixel.util.FlxGradient;
 import flixel.FlxState;
 import flixel.FlxBasic;
+#if android
+import flixel.input.actions.FlxActionInput;
+import flixel.FlxCamera;
+import android.FlxVirtualPad;
+import android.FlxHitbox;
+#end
 
 class MusicBeatState extends FlxUIState
 {
@@ -48,6 +54,45 @@ class MusicBeatState extends FlxUIState
 		}
 		
 		FlxTransitionableState.skipNextTransOut = false;
+	}
+	
+	#if android
+	var _virtualpad:FlxVirtualPad;
+	var _hitbox:FlxHitbox;
+	var trackedinputsUI:Array<FlxActionInput> = [];
+	var trackedinputsNOTES:Array<FlxActionInput> = [];
+	#end
+	#if android
+	public function addVirtualPad(DPad:FlxDPadMode, Action:FlxActionMode) {
+		_virtualpad = new FlxVirtualPad(DPad, Action);
+		add(_virtualpad);
+		controls.setVirtualPadUI(_virtualpad, DPad, Action);
+		trackedinputsUI = controls.trackedinputsUI;
+		controls.trackedinputsUI = [];
+	}
+	#end
+		
+	#if android
+	public function removeVirtualPad() {
+		controls.removeFlxInput(trackedinputsUI);
+		remove(_virtualpad);
+	}
+	#end
+
+	#if android
+        public function addPadCamera() {
+		var camcontrol = new flixel.FlxCamera();
+		FlxG.cameras.add(camcontrol);
+		camcontrol.bgColor.alpha = 0;
+		_virtualpad.cameras = [camcontrol];
+	}
+	#end
+	
+	override function destroy() {
+		#if android
+		controls.removeFlxInput(trackedinputsUI);
+		controls.removeFlxInput(trackedinputsNOTES);	
+		#end
 	}
 
 	override function update(elapsed:Float)
