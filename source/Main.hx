612b894a5a5e6d48ca3a11a2d39a1c9f5fe9922b
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2023-06-06 13:26:14 +0800

    readding fps counter
    cos i wanna see my fps!1111!11!!!!!!11!11!!!

--- source/Main.hx
+++ source/Main.hx
@@ -98,7 +98,7 @@ class Main extends Sprite
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 		addChild(game);
 	
-		#if !mobile
+		
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
@@ -108,13 +108,6 @@ class Main extends Sprite
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.showFPS;
 		}
-		#end
-
-
-
-
-
-		
 
 		#if html5
 		FlxG.autoPause = false;
